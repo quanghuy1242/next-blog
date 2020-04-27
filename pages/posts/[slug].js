1362429d0d3ba34991009afc08f17f0d150889a5
Quang Huy <quanghuy1242@gmail.com>
2020-04-27 16:33:23 +0700

    trigger  deploy

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -12,7 +12,7 @@ import PostTitle from '../../components/post-title'
 import Head from 'next/head'
 import { CMS_NAME } from '../../lib/constants'
 import markdownToHtml from '../../lib/markdownToHtml'
-import { renderMetaTags } from 'react-datocms';
+import { renderMetaTags } from 'react-datocms'; 
 
 export default function Post({ post, morePosts, preview }) {
   const router = useRouter()
