4ac7b99bd931553ec3742b081b363a0622821b35
Nguyen Quang Huy <quanghuy1242@gmail.com>
2021-07-25 15:04:05 +0700

    adding bundler analyzier

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 
-module.exports = {
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({
   env: {
     NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN:
       process.env.NEXT_EXAMPLE_CMS_DATOCMS_API_TOKEN,
   },
-}
+})
